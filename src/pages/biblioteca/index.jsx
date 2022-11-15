663fe4f7831eee38690367f15bdd617d26cb04a0
Maurilio Menezes <mauriliosm25@gmail.com>
2022-11-15 12:43:52 -0300

    feat : áudio e historia adicionados no modal

--- src/pages/biblioteca/index.jsx
+++ src/pages/biblioteca/index.jsx
@@ -17,33 +17,97 @@ const Biblioteca = () => {
           <Row>
             <Col>
               <Modals
-                img="img/biblioteca/urso.jpg"
-                titulo="Os viajantes e o urso" />
+                img="img/biblioteca/Ovos-de-ouro.jpg"
+                titulo="Ovos de Ouro"
+                audio="/audio/OvosDeOuro.mpeg"
+                texto="		Um homem e sua mulher tinham a sorte de possuir uma gansa que
+                todos os dias punha um ovo de ouro. Mesmo com toda essa sorte,
+                eles acharam que estavam enriquecendo muito devagar, que assim
+                não dava... Imaginando que a gansa devia ser de ouro por
+                dentro, resolveram matá-la e pegar aquela fortuna toda de uma
+                vez. Só que, quando abriram a barriga da gansa, viram que por
+                dentro ela era igualzinha a todas as outras. Foi assim que os
+                dois não ficaram ricos de uma vez só, como tinham imaginado,
+                nem puderam continuar recebendo o ovo de ouro que todos os dias
+                aumentava um pouquinho sua fortuna." />
             </Col>
             <Col>
               <Modals
                 img="img/biblioteca/calunia.jpg"
-                titulo="Calúnia" />
+                titulo="Calúnia"
+                audio="/audio/calunia.mpeg"
+                texto="	Uma mulher tanto falou que seu vizinho era um ladrão que o
+                rapaz acabou sendo preso. Dias depois, descobriram que ele era
+                inocente. O rapaz, então, foi solto e processou a mulher. —
+                Comentários não causam tanto mal, disse ela em sua defesa
+                diante do tribunal. — Escreva os comentários num papel, depois
+                pique-o e jogue os pedaços no caminho de casa. Amanhã, volte
+                para ouvir a sentença, respondeu o juiz. A mulher obedeceu e
+                voltou no dia seguinte. — Antes da sentença, terá que
+                recolher todos os pedaços de papel que espalhou ontem, disse o
+                juiz. — Impossível, respondeu ela. Já não sei onde estão. —
+                Da mesma maneira, um simples comentário pode destruir a honra
+                de um homem, e depois você não tem como consertar o mal,
+                respondeu o juiz, condenando a mulher à prisão." />
             </Col> <Col>
               <Modals
-                img="img/biblioteca/formiga.jpg"
-                titulo="Formigas" />
+                img="img/biblioteca/urso.jpg"
+                titulo="Os viajantes e o urso"
+                audio="/audio/viajantesUrso.mpeg"
+                texto="	Dois homens viajavam juntos quando, de repente, surgiu um urso
+                de dentro da floresta e parou diante deles, urrando. Um dos
+                homens tratou de subir na árvore mais próxima e agarrar-se
+                aos ramos. O outro, vendo que não tinha tempo para
+                esconder-se, deitou-se no chão, esticado, fingindo de morto,
+                porque ouvira dizer que os ursos não tocam em homens mortos. O
+                urso aproximou-se, cheirou o homem deitado, e voltou de novo
+                para a floresta. Quando a fera desapareceu, o homem da árvore
+                desceu apressadamente e disse ao companheiro: — Vi o urso a
+                dizer alguma coisa no teu ouvido. Que foi que ele disse? Disse
+                que eu nunca viajasse com um medroso." />
             </Col>
           </Row>
           <Row>
             <Col>
               <Modals
                 img="img/biblioteca/CaoEosso.jpg"
-                titulo="O cão e o osso" />
+                titulo="O cão e o osso"
+                audio="/audio/caoeosso.mpeg"
+                texto="	Um dia, um cão ia atravessando uma ponte, carregando um osso
+                na boca. Olhando para baixo, viu sua própria imagem refletida
+                na água. Pensando ver outro cão, cobiçou-lhe logo o osso e
+                pôs-se a latir. Mal, porém, abriu a boca, seu próprio osso
+                caiu na água e se perdeu para sempre." />
             </Col>
             <Col>
               <Modals
                 img="img/biblioteca/estreladoMar.jpg"
-                titulo="Estrela do mar" />
+                titulo="Estrela do mar"
+                audio="/audio/EstrelaDoMar.mpeg"
+                texto="	Havia um homem que morava numa bela praia, junto a uma colônia
+                de pescadores. Num dos seus passeios matinais, ele viu um jovem
+                jogando de volta ao oceano as estrelas-do-mar que estavam na
+                areia. — Por que você faz isso?, perguntou o homem. Porque a
+                maré está baixa, e elas vão morrer. — Meu jovem, existem
+                milhares de quilômetros de praia por este mundo e centenas de
+                milhares de estrelas-do-mar espalhadas pela areia. Que
+                diferença você pode fazer? O jovem pegou mais uma estrela e
+                atirou-a no oceano. Depois virou-se para o homem respondendo: —
+                Para esta, eu fiz uma grande diferença. " />
             </Col> <Col>
               <Modals
                 img="img/biblioteca/LeaoeJavali.webp"
-                titulo="Leao e Javali" />
+                titulo="Leao e Javali"
+                audio="/audio/leaojavali.mpeg"
+                texto="		Num dia muito quente, um leão e um javali chegaram juntos a um
+                poço. Estavam com muita sede e começaram a discutir para ver
+                quem beberia primeiro. Nenhum cedia a vez ao outro. Já iam
+                atracar-se para brigar, quando o leão olhou para cima e viu
+                vários urubus voando. — Olhe lá! — disse o leão. — Aqueles
+                urubus estão com fome e esperam para ver qual de nós dois
+                será derrotado. — Então, é melhor fazermos as pazes —
+                respondeu o javali. — Prefiro ser seu amigo a ser comida de
+                urubus." />
             </Col>
           </Row>
 
@@ -51,16 +115,46 @@ const Biblioteca = () => {
             <Col>
               <Modals
                 img="img/biblioteca/râ.jpg"
-                titulo="Rã" />
+                titulo="Rã"
+                audio="/audio/RâTouro.mpeg"
+                texto="	Um grande touro passeava pela margem de um riacho. A rã ficou
+                com muita inveja de seu tamanho e de sua força. Então,
+                começou a inchar, fazendo enorme esforço, para tentar ficar
+                tão grande quanto o touro. Perguntou às companheiras do
+                riacho se estava do tamanho do touro. Elas responderam que
+                não. A rã tornou a inchar e inchar, mas, ainda assim, não
+                alcançou o tamanho do touro. Pela terceira vez, a rã tentou
+                inchar. Mas fez isso com tanta força que acabou explodindo,
+                por culpa de tanta inveja." />
             </Col>
             <Col>
               <Modals
                 img="img/biblioteca/raposa.webp"
-                titulo="A raposa" />
+                titulo="A raposa"
+                audio="/audio/raposaUVa.mpeg"
+                texto="	Uma raposa passou embaixo de um pé carregado com lindas uvas.
+                Ficou com muita vontade de comer aquelas uvas. Deu muitos
+                saltos, tentou subir na parreira, mas não conseguiu. Depois de
+                muito tentar foi-se embora, dizendo: — Eu nem estou ligando
+                para as uvas. Elas estão verdes, mesmo..." />
             </Col> <Col>
               <Modals
                 img="img/biblioteca/CigarraeFormiga.webp"
-                titulo="A cigarra e a formiga" />
+                titulo="A cigarra e a formiga"
+                audio="/audio/cigarraFormiga.mpeg"
+                texto="	Num belo dia de inverno as formigas estavam tendo o maior
+                trabalho para secar suas reservas de trigo. Depois de uma
+                chuvarada, os grãos tinham ficado completamente molhados. De
+                repente, apareceu uma cigarra: — Por favor, formiguinhas, me
+                dêem um pouco de trigo! Estou com uma fome danada, acho que
+                vou morrer. As formigas pararam de trabalhar, coisa que era
+                contra os princípios delas, e perguntaram: — Mas por quê? O
+                que você fez durante o verão? Por acaso não se lembrou de
+                guardar comida para o inverno? — Para falar a verdade, não
+                tive tempo — respondeu a cigarra. — Passei o verão cantando! —
+                Bom. Se você passou o verão cantando, que tal passar o inverno
+                dançando? — disseram as formigas, e voltaram para o trabalho
+                dando risada." />
             </Col>
           </Row>
 
