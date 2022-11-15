95e9bcf327c10558ff340a4c5c1bdef1f0ec4515
Maurilio Menezes <mauriliosm25@gmail.com>
2022-11-15 12:40:12 -0300

    fix : erros no console

--- src/pages/videoAula/index.jsx
+++ src/pages/videoAula/index.jsx
@@ -2,7 +2,10 @@ import { Button, Col, Container, Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import Aprender from "../../components/aprender"
 import Cards from "../../components/Cards"
+import Footer from "../../components/footer"
 import Header from "../../components/Header"
+import LinkPages from "../../components/LinksExercicios"
+import PagExe from "../../components/LinksExercicios"
 import "./videoAula.css"
 
 
@@ -23,20 +26,20 @@ const VideoAula1 = () => {
             <Row>
               <Col>
                 <Cards
-                  video={<iframe width="100%" height="225" class="video-p" src="https://www.youtube.com/embed/hRtd_2LtM14"
-                    title="YouTube video player" frameborder="0"
+                  video={<iframe width="100%" height="225" className="video-p" src="https://www.youtube.com/embed/hRtd_2LtM14"
+                    title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>}
+                    allowFullScreen></iframe>}
                   titulo="Aula 01"
                   texto={"O vovô Manu está na área e vai ensinar para vocês um jeito divertido de aprender português e matemática!"}
                 />
               </Col>
               <Col>
                 <Cards
-                  video={<iframe width="100%" height="225" class="video-p" src="https://www.youtube.com/embed/_N3Ny10OHQQ"
-                    title="YouTube video player" frameborder="0"
+                  video={<iframe width="100%" height="225" className="video-p" src="https://www.youtube.com/embed/_N3Ny10OHQQ"
+                    title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>}
+                    allowFullScreen></iframe>}
                   titulo="Aula 02"
                   texto={"O vovô Manu está na área e vai ensinar para vocês um jeito divertido de aprender português e matemática!"}
                 />
@@ -86,6 +89,7 @@ const VideoAula1 = () => {
 
 
         </Container>
+        <Footer />
       </main >
     </>
   )
